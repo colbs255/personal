0f08dbb9ebba5df68fa938cc5858ab8919f0f5a5
colbs255 <colby@dev.com>
2025-03-29 18:40:35 -0400

    Add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,71 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+const name = "/Colby/Chance";
+const joke = "/Colby/Cheese";
+const common = "/Colby/Ch"
+
+// 1) Type the joke
+// 2) Backspace the joke to the common prefix
+// 3) Type name correctly
+const State = {
+  FORWARD_JOKE: 0,
+  BACKWARD_JOKE: 1,
+  FORWARD_NAME: 2,
+  DONE: 3,
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  // Initialize to "/" so the element doesn't pop in
+  const [displayedText, setDisplayedText] = useState("/");
+  const [state, setState] = useState(State.FORWARD_JOKE);
+  useEffect(() => {
+    setTimeout(() => {
+      switch (state) {
+        case State.FORWARD_JOKE: {
+          if (displayedText === joke) {
+            setState(State.BACKWARD_JOKE);
+          } else {
+            setDisplayedText((prev) => joke.slice(0, prev.length + 1));
+          }
+          break;
+        }
+        case State.BACKWARD_JOKE: {
+          if (displayedText === common) {
+            setState(State.FORWARD_NAME);
+          } else {
+            setDisplayedText((prev) => joke.slice(0, prev.length - 1));
+          }
+          break;
+        }
+        case State.FORWARD_NAME: {
+          if (displayedText === name) {
+            setState(State.DONE);
+          } else {
+            setDisplayedText((prev) => name.slice(0, prev.length + 1));
+          }
+          break;
+        }
+        case State.DONE: {
+          // do nothing
+          break;
+        }
+        default: {
+          throw new Error(`Unhandled state`);
+        }
+      }
+    }, 100);
+  }, [displayedText, state]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        {displayedText}
+      </h1>
+       <p className="mb-4">
+        {`Hi! This is my personal site where I host my writings and talks.`}
+      </p>
     </div>
   );
 }
