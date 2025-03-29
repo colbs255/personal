ca8c631bff1634b2282fc39b019dd3b9e2cb9c25
colbs255 <colby@dev.com>
2025-03-29 18:38:01 -0400

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs
+            pkgs.corepack
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
