2a6b8d0ce3abe70cd4fb041229f942308a07f882
colbs255 <colby@dev.com>
2025-03-29 18:41:18 -0400

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        About Me
+      </h1>
+       <p className="mb-4">
+        {`I am a software engineer.`}
+      </p>
+    </div>
+  );
+}
