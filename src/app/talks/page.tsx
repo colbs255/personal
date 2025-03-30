ee48131ce417950f885264c931b2f9c94e292970
colbs255 <colby@dev.com>
2025-03-30 16:33:36 -0400

    Open presentations in new tab

--- src/app/talks/page.tsx
+++ src/app/talks/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Page() {
     const filePath = path.join(process.cwd(), "public", "talks", "index.json");
-    const index = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    const index: Record<string, string> = JSON.parse(fs.readFileSync(filePath, "utf8"));
     return (
         <div>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
@@ -17,6 +17,8 @@ export default function Page() {
                         key={slug}
                         className="flex flex-col space-y-1 mb-4"
                         href={`/talks/${slug}`}
+                        rel="noopener noreferrer"
+                        target="_blank"
                     >
                         <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
                             <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
