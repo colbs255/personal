d7e5f10dd6f34edeb2375fa090739cddb6a3b3b6
colbs255 <colby@dev.com>
2025-03-30 16:20:32 -0400

    Format

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,8 +1,11 @@
 import { Doc } from "@/lib/doc";
-import styles from "./post.module.css"
+import styles from "./post.module.css";
 
 export default function Page(doc: Doc) {
-   return (
-    <div className={styles.post} dangerouslySetInnerHTML={{ __html: doc.content }} />
-  );
+    return (
+        <div
+            className={styles.post}
+            dangerouslySetInnerHTML={{ __html: doc.content }}
+        />
+    );
 }
--- src/app/writings/page.tsx
+++ src/app/writings/page.tsx
@@ -1,47 +1,49 @@
-"use client"
-import Link from 'next/link'
-import { Metadata } from '@/lib/doc';
+"use client";
+import Link from "next/link";
+import { Metadata } from "@/lib/doc";
 import { useEffect, useState } from "react";
-import Input from '../components/input';
+import Input from "../components/input";
 
 export default function Page() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [posts, setDocs] = useState<Metadata[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [posts, setDocs] = useState<Metadata[]>([]);
 
-  useEffect(() => {
-    fetch("writings/index.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch JSON');
-        }
-        return response.json();
-      })
-      .then((jsonData) => { setDocs(jsonData.data) })
-  }, []);
+    useEffect(() => {
+        fetch("writings/index.json")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch JSON");
+                }
+                return response.json();
+            })
+            .then((jsonData) => {
+                setDocs(jsonData.data);
+            });
+    }, []);
 
-  return (
-    <div>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Writings
-      </h1>
-      <Input placeholder="Filter..." onChange={setSearchTerm} />
-      <div>
-        {posts
-          .filter(v => v.title.toLowerCase().includes(searchTerm))
-          .map(v => (
-            <Link
-              key={v.slug}
-              className="flex flex-col space-y-1 mb-4"
-              href={`/writings/${v.slug}`}
-            >
-              <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                  {v.title} {v.tags.join(",")}
-                </p>
-              </div>
-            </Link>
-          ))}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                Writings
+            </h1>
+            <Input placeholder="Filter..." onChange={setSearchTerm} />
+            <div>
+                {posts
+                    .filter((v) => v.title.toLowerCase().includes(searchTerm))
+                    .map((v) => (
+                        <Link
+                            key={v.slug}
+                            className="flex flex-col space-y-1 mb-4"
+                            href={`/writings/${v.slug}`}
+                        >
+                            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+                                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                                    {v.title} {v.tags.join(",")}
+                                </p>
+                            </div>
+                        </Link>
+                    ))}
+            </div>
+        </div>
+    );
 }
