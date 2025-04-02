c14da19a08455381f7cf00c20a327fa070438a34
colbs255 <colby@dev.com>
2025-04-02 00:37:38 -0400

    save

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,9 +1,19 @@
 import { Doc } from "@/lib/doc";
 import { evaluate } from "@mdx-js/mdx";
 import * as runtime from "react/jsx-runtime";
+import hljs from 'highlight.js';
+import "./highlightjs.css"
+
+
+function Code({ children, ...props }) {
+  const language = props.className.replace("language-", "");
+  const codeHTML = hljs.highlight(children, { language }).value;
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+}
 
 const components = {
     h2: (props: any) => <h2 className="mb-8 text-xl font-semibold" {...props} />,
+    code: Code,
 }
 
 export default async function Page(doc: Doc) {
