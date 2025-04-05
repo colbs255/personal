20daf624304cebd392109808786d239ed43cfa97
colbs255 <colby@dev.com>
2025-04-05 15:37:33 -0400

    Setup static deployment

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -2,15 +2,20 @@ import { Doc } from "@/lib/doc";
 import { evaluate } from "@mdx-js/mdx";
 import * as runtime from "react/jsx-runtime";
 import hljs from "highlight.js";
+import { HTMLAttributes } from "react";
 
-function Code({ children, ...props }) {
-    const language = props.className.replace("language-", "");
+type Props = {
+    children: string;
+} & HTMLAttributes<HTMLElement>;
+
+function Code({ children, className = "", ...props }: Props) {
+    const language = className.replace("language-", "");
     const codeHTML = hljs.highlight(children, { language }).value;
     return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
 
 const components = {
-    h2: (props: any) => (
+    h2: (props: Props) => (
         <h2 className="mb-8 text-xl font-semibold" {...props} />
     ),
     code: Code,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,6 @@ export const metadata: Metadata = {
     description: "My personal website",
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(" ");
-
 export default function RootLayout({
     children,
 }: {
@@ -21,11 +19,7 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            className={cx(
-                "text-black bg-white dark:text-white dark:bg-black",
-                GeistSans.variable,
-                GeistMono.variable,
-            )}
+            className={`text-black bg-white dark:text-white dark:bg-black ${GeistSans.variable} ${GeistMono.variable}`}
         >
             <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
                 <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
