be8e625bc65cb10d068205d3bd8565ff6c25b1e3
colbs255 <colby@dev.com>
2025-04-06 16:04:32 -0400

    Rename presentations -> slides

--- src/app/slides/page.tsx
+++ src/app/slides/page.tsx
@@ -4,22 +4,22 @@ import Grid, { Item } from "../components/grid";
 import { Metadata } from "@/lib/types";
 
 export default function Page() {
-    const filePath = path.join(process.cwd(), "public", "talks", "index.json");
+    const filePath = path.join(process.cwd(), "public", "slides", "index.json");
     const index: Metadata[] = JSON.parse(
         fs.readFileSync(filePath, "utf8"),
     ).data;
     const items: Item[] = index.map((v) => ({
         name: v.title,
         date: v.publishedAt,
-        href: `/talks/${v.slug}`,
+        href: `/slides/${v.slug}`,
         tags: v.tags,
     }));
     return (
         <div>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-                Talks
+                Slides
             </h1>
-            <p className="mb-4">Some of my talks:</p>
+            <p className="mb-4">Some of my slides:</p>
             <Grid items={items} openInNewTab={true} />
         </div>
     );
