a56f5bb3428e310e38caf7cef7c5557cbd1ad75b
colbs255 <colby@dev.com>
2025-04-08 20:58:56 -0400

    Posts page: server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,26 +1,15 @@
-"use client";
+import fs from "fs";
+import path from "path";
 import { Metadata } from "@/lib/types";
-import { useEffect, useState } from "react";
 import Grid, { Item } from "../components/grid";
 import { compareLocalDate } from "@/lib/util";
 import Title from "../components/title";
 
 export default function Page() {
-    const [posts, setDocs] = useState<Metadata[]>([]);
-
-    useEffect(() => {
-        fetch("posts/index.json")
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Failed to fetch JSON");
-                }
-                return response.json();
-            })
-            .then((jsonData) => {
-                setDocs(jsonData.data);
-            });
-    }, []);
-
+    const filePath = path.join(process.cwd(), "public", "posts", "index.json");
+    const posts: Metadata[] = JSON.parse(
+        fs.readFileSync(filePath, "utf8"),
+    ).data;
     const items: Item[] = posts
         .map((v: Metadata) => ({
             name: v.title,
