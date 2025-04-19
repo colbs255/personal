d4a888a78fc0a1075ba282bb13a14745cbc22256
colbs255 <colbs255@users.noreply.github.com>
2025-04-19 15:18:37 -0400

    save

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -9,11 +9,32 @@ type AdmonitionProps = {
     children: React.ReactNode;
 };
 
-function Info(props: AdmonitionProps) {
+enum AdmonitionType {
+    Info,
+    Warning,
+};
+
+type AdmonitionSchema = {
+    defaultTitle: string,
+    backgroundColor: string,
+};
+
+const admonitions: Record<AdmonitionType, AdmonitionSchema> = {
+    [AdmonitionType.Info]: {
+        defaultTitle: "Info",
+        backgroundColor: "bg-blue-500",
+    },
+    [AdmonitionType.Warning]: {
+        defaultTitle: "Warning",
+        backgroundColor: "bg-blue-500",
+    },
+}
+
+function Admonition(schema: AdmonitionSchema, props: AdmonitionProps) {
     return (
         <div className="border border-blue-300 rounded-lg overflow-hidden shadow-sm">
             <div className="bg-blue-500 text-white font-semibold px-4 py-2 text-sm">
-                {props.title ?? "Info"}
+                {props.title ?? schema.defaultTitle}
             </div>
             {props.children}
         </div>
@@ -21,7 +42,10 @@ function Info(props: AdmonitionProps) {
 }
 
 function mdxComponents() {
-    return { Info };
+    return {
+        Info: (props: AdmonitionProps) => Admonition(admonitions[AdmonitionType.Info], props),
+        Warning: (props: AdmonitionProps) => Admonition(admonitions[AdmonitionType.Warning], props),
+    };
 }
 
 export default async function Page(doc: Doc) {
