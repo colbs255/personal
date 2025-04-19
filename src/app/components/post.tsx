918960af7f08e45c1dd08d92f8d13ef4f38a22bf
colbs255 <colbs255@users.noreply.github.com>
2025-04-19 00:52:59 -0400

    save

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -1,21 +1,21 @@
 import { Doc } from "@/lib/types";
-import { evaluate } from '@mdx-js/mdx'
-import * as runtime from 'react/jsx-runtime'
+import { evaluate } from "@mdx-js/mdx";
+import * as runtime from "react/jsx-runtime";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 
 type AdmonitionProps = {
     title?: string;
     children: React.ReactNode;
-}
+};
 
 function Info(props: AdmonitionProps) {
     return (
         <div className="border border-blue-300 rounded-lg overflow-hidden shadow-sm">
             <div className="bg-blue-500 text-white font-semibold px-4 py-2 text-sm">
-            {props.title ?? "Info"}
+                {props.title ?? "Info"}
             </div>
-            { props.children }
+            {props.children}
         </div>
     );
 }
