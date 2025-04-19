c872ffeedd06fdb759741d4f099a5769453939a7
colbs255 <colbs255@users.noreply.github.com>
2025-04-19 16:26:51 -0400

    save

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -5,11 +5,25 @@ import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 
 function Info(props: AdmonitionProps) {
-    return Admonition({ defaultTitle: "Info", titleColor: "bg-blue-900", borderColor: "border-blue-500", }, props);
+    return Admonition(
+        {
+            defaultTitle: "Info",
+            titleColor: "bg-blue-700",
+            borderColor: "border-blue-400",
+        },
+        props,
+    );
 }
 
 function Warning(props: AdmonitionProps) {
-    return Admonition({ defaultTitle: "Warning", titleColor: "bg-yellow-900", borderColor: "border-yellow-500", }, props);
+    return Admonition(
+        {
+            defaultTitle: "Warning",
+            titleColor: "bg-yellow-700",
+            borderColor: "border-yellow-400",
+        },
+        props,
+    );
 }
 
 type AdmonitionProps = {
@@ -18,20 +32,22 @@ type AdmonitionProps = {
 };
 
 type AdmonitionSchema = {
-    defaultTitle: string,
-    titleColor: string,
-    borderColor: string,
+    defaultTitle: string;
+    titleColor: string;
+    borderColor: string;
 };
 
 function Admonition(schema: AdmonitionSchema, props: AdmonitionProps) {
     return (
-        <div className={`border ${schema.borderColor} rounded-lg overflow-hidden shadow-sm`}>
-            <div className={`${schema.titleColor} text-white font-semibold px-4 py-2 text-sm`}>
+        <div
+            className={`border-2 ${schema.borderColor} rounded-lg overflow-hidden shadow-sm mb-2`}
+        >
+            <div
+                className={`${schema.titleColor} text-white font-semibold px-4 py-2 text-sm`}
+            >
                 {props.title ?? schema.defaultTitle}
             </div>
-            <div className="px-3">
-                {props.children}
-            </div>
+            <div className="px-3">{props.children}</div>
         </div>
     );
 }
