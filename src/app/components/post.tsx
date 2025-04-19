e76aef664125ddda98b83ee7fd887f42d9a9885e
colbs255 <colbs255@users.noreply.github.com>
2025-04-19 15:35:24 -0400

    save

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -4,31 +4,23 @@ import * as runtime from "react/jsx-runtime";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 
+function Info(props: AdmonitionProps) {
+    return Admonition({ defaultTitle: "Info", backgroundColor: "bg-blue-500", }, props);
+}
+
+function Warning(props: AdmonitionProps) {
+    return Admonition({ defaultTitle: "Warning", backgroundColor: "bg-blue-500", }, props);
+}
+
 type AdmonitionProps = {
     title?: string;
     children: React.ReactNode;
-};
-
-enum AdmonitionType {
-    Info,
-    Warning,
 };
 
 type AdmonitionSchema = {
     defaultTitle: string,
     backgroundColor: string,
 };
-
-const admonitions: Record<AdmonitionType, AdmonitionSchema> = {
-    [AdmonitionType.Info]: {
-        defaultTitle: "Info",
-        backgroundColor: "bg-blue-500",
-    },
-    [AdmonitionType.Warning]: {
-        defaultTitle: "Warning",
-        backgroundColor: "bg-blue-500",
-    },
-}
 
 function Admonition(schema: AdmonitionSchema, props: AdmonitionProps) {
     return (
@@ -43,8 +35,8 @@ function Admonition(schema: AdmonitionSchema, props: AdmonitionProps) {
 
 function mdxComponents() {
     return {
-        Info: (props: AdmonitionProps) => Admonition(admonitions[AdmonitionType.Info], props),
-        Warning: (props: AdmonitionProps) => Admonition(admonitions[AdmonitionType.Warning], props),
+        Info,
+        Warning,
     };
 }
 
