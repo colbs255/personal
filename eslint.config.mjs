55118a13d59fe3ee9a8c3e67df1c9984d81ceb86
colbs255 <colbs255@users.noreply.github.com>
2025-04-20 17:16:32 -0400

    save

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@next/next/no-img-element": "off",
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@next/next/no-img-element": "off",
+        },
     },
-  },
 ];
 
 export default eslintConfig;
