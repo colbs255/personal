b8df9b31fccd93c4851a9987481cccdcf8030b85
colbs255 <colbs255@users.noreply.github.com>
2025-04-21 20:58:34 -0400

    Move admonition to component file

---
+++ src/app/components/admonition.tsx
@@ -0,0 +1,28 @@
+export type AdmonitionProps = {
+    title?: string;
+    children: React.ReactNode;
+};
+
+export type AdmonitionSchema = {
+    defaultTitle: string;
+    titleColor: string;
+    borderColor: string;
+};
+
+export default function Admonition(
+    schema: AdmonitionSchema,
+    props: AdmonitionProps,
+) {
+    return (
+        <div
+            className={`border-2 ${schema.borderColor} rounded-lg overflow-hidden shadow-sm mb-2`}
+        >
+            <div
+                className={`${schema.titleColor} text-white font-semibold px-4 py-2 text-sm`}
+            >
+                {props.title ?? schema.defaultTitle}
+            </div>
+            <div className="px-3">{props.children}</div>
+        </div>
+    );
+}
--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -4,32 +4,7 @@ import * as runtime from "react/jsx-runtime";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 import PlantUML from "./plantuml";
-
-type AdmonitionProps = {
-    title?: string;
-    children: React.ReactNode;
-};
-
-type AdmonitionSchema = {
-    defaultTitle: string;
-    titleColor: string;
-    borderColor: string;
-};
-
-function Admonition(schema: AdmonitionSchema, props: AdmonitionProps) {
-    return (
-        <div
-            className={`border-2 ${schema.borderColor} rounded-lg overflow-hidden shadow-sm mb-2`}
-        >
-            <div
-                className={`${schema.titleColor} text-white font-semibold px-4 py-2 text-sm`}
-            >
-                {props.title ?? schema.defaultTitle}
-            </div>
-            <div className="px-3">{props.children}</div>
-        </div>
-    );
-}
+import Admonition, { AdmonitionProps } from "./admonition";
 
 function mdxComponents() {
     return {
