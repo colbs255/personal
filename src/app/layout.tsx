a78f4ce11fc45388c8016d9ca7c04e7f31b5dfe5
colbs255 <colbs255@users.noreply.github.com>
2025-04-26 19:06:57 -0400

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import "./highlightjs.css";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
