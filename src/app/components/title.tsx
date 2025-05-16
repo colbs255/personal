7c656c23b3dda66308e52edf5f43086006d4f3b5
colbs255 <colbs255@users.noreply.github.com>
2025-05-15 22:22:52 -0400

    | Style | Headers (#91)

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -5,9 +5,5 @@ type TitleProps = {
 };
 
 export default function Title({ children }: TitleProps) {
-    return (
-        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-            {children}
-        </h1>
-    );
+    return <h1>{children}</h1>;
 }
