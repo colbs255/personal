63b6e30547da1bc85307bb508c1dd4d11a107c04
colbs255 <colbs255@users.noreply.github.com>
2025-05-17 16:16:58 -0400

    Format subheading

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 type TitleProps = {
+    subheading?: string;
     children: React.ReactNode;
 };
 
-export default function Title({ children }: TitleProps) {
-    return <h1>{children}</h1>;
+export default function Title(props: TitleProps) {
+    return (
+        <div className="mb-8">
+            <h1>{props.children}</h1>
+            {props.subheading && (
+                <p className="mt-1 text-sm text-neutral-600 dark:text-neutral-400">
+                    {props.subheading}
+                </p>
+            )}
+        </div>
+    );
 }
