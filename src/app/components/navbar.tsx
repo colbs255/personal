accd558e111d451c46379300fa67b2a1020e6a92
colbs255 <colbs255@users.noreply.github.com>
2025-05-18 16:38:37 -0400

    save

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -26,7 +26,7 @@ export function Navbar() {
                                 <Link
                                     key={path}
                                     href={path}
-                                    className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                                    className="transition-all text-neutral-500 hover:text-black dark:text-neutral-500 dark:hover:text-white flex align-middle relative py-1 px-2 m-1"
                                 >
                                     {name}
                                 </Link>
